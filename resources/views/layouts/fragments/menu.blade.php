f77903598b3637b691fb9e7dd08401e0b119ece5
Angel_Salazar <angel_salazar2017@hotmail.com>
2020-07-19 00:11:51 -0500

    funcionalidad del panel

---
+++ resources/views/layouts/fragments/menu.blade.php
@@ -0,0 +1,29 @@
+<ul class="nav nav-primary">
+    <li class="nav-section">
+        <span class="sidebar-mini-icon">
+            <i class="fa fa-ellipsis-h"></i>
+        </span>
+        <h4 class="text-section">Menú</h4>
+    </li>
+    <li class="nav-item">
+        <a href="#base">
+            <i class="fas fa-layer-group"></i>
+            <p>Clientes</p>
+            {{--<span class="caret"></span>--}}
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="#base">
+            <i class="fas fa-layer-group"></i>
+            <p>Dependencias</p>
+            {{--<span class="caret"></span>--}}
+        </a>
+        <li class="nav-item">
+            <a href="#base">
+                <i class="fas fa-layer-group"></i>
+                <p>Usuarios del sistema</p>
+                {{--<span class="caret"></span>--}}
+            </a>
+        </li>
+    </li>
+</ul>
